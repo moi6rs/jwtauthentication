9ba8e7dd341db9bbdbc4c2ac97830a5374973f8d
mramoss <moi6ramoss@gmail.com>
2019-12-19 11:41:14 -0400

    Spring Boot atentificacion con JWT

---
+++ jwtauthentication/src/main/java/com/ideasconnector/jwtauthentication/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.ideasconnector.jwtauthentication.repository;
+
+import com.ideasconnector.jwtauthentication.model.Role;
+import com.ideasconnector.jwtauthentication.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
